6b2ff89c920d4242189a92d2499330f4fe271c5d
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-01-06 13:47:36 +0530

    commit

--- src/main/java/com/example/employeems/service/EmployeeService.java
+++ src/main/java/com/example/employeems/service/EmployeeService.java
@@ -1,4 +1,19 @@
 package com.example.employeems.service;
 
+import com.example.employeems.dto.EmployeeDTO;
+import com.example.employeems.repo.EmployeeRepo;
+import com.example.employeems.util.VarList;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+    public String saveEmployee(EmployeeDTO employeeDTO){
+        if(employeeRepo.existsById(employeeDTO.getEmpID())){
+            return VarList.RSP_DUPLICATED;
+        }else {
+            employeeRepo.save(employeeDTO);
+        }
+    }
 }
