24e1d9ee41a1a966e0a99573c2e5e76b55ad614f
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-01-09 22:18:20 +0530

    commit

--- src/main/java/com/example/employeems/service/EmployeeService.java
+++ src/main/java/com/example/employeems/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.example.employeems.entity.Employee;
 import com.example.employeems.repo.EmployeeRepo;
 import com.example.employeems.util.VarList;
 import jakarta.transaction.Transactional;
+import org.aspectj.weaver.ast.Var;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
@@ -31,9 +32,10 @@ public class EmployeeService {
 
     public String updateEmployee(EmployeeDTO employeeDTO){
         if(employeeRepo.existsById(employeeDTO.getEmpID())){
-
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.RSP_SUCCESS;
         }else{
-
+            return VarList.RSP_NO_DATA_FOUND;
         }
     }
 }
