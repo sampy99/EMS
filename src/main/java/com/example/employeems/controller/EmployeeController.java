e2438b98aafc35b3db9ee5d64ce6fdbd25a6645f
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-01-12 23:49:42 +0530

    commit

--- src/main/java/com/example/employeems/controller/EmployeeController.java
+++ src/main/java/com/example/employeems/controller/EmployeeController.java
@@ -97,6 +97,25 @@ public class EmployeeController {
 
     @GetMapping("/searchEmployee/{empID}")
     public ResponseEntity searchEmployee(@PathVariable int empID){
+        try{
+            EmployeeDTO employeeDTO = employeeService.searchEmployee(empID);
+            if(employeeDTO != null){
+                responseDTO.setCode(VarList.RSP_SUCCESS);
+                responseDTO.setMessage("Success");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+            }else{
+                responseDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+                responseDTO.setMessage("No employee is available for this empID");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+            }
+        }catch (Exception e){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(e.getMessage());
+            responseDTO.setContent(e);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
 
